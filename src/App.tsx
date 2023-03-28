2b0e4413f40bf7571563aabf7f42ac4bf3fb4c6b
okxooxoo <okxooxoo@naver.com>
2023-03-28 23:44:13 +0900

    first commit by okxooxoo

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h3>I cloned it!</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
