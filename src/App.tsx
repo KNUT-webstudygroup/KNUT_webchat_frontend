937c4d8e5bb464c443933538f4beb36f6d2a3f38
LuticaCANARD <presan100@gmail.com>
2023-04-04 14:52:24 +0900

    fix : added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './app.css'
+import React, { Component,useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './login'
+import Register from './register'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const fronturi = 'KNUT_webchat_frontend'
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h3>I cloned it!</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route path={fronturi+"/"} element={<Login />}></Route>
+					<Route path={fronturi+"/regist/"} element={<Register />}></Route>
+					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+					{/*<Route path="*" element={<NotFound />}></Route>*/}
+				</Routes>
+			</BrowserRouter>
+		</div>
   )
 }
 
