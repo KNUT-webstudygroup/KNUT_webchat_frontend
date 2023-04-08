55556456fb37208d1944840aea6f8b7f55f7ec63
Lutica_CANARD <presan100@gmail.com>
2023-04-08 10:42:31 +0900

    fix : change on realserver

--- src/logics.ts
+++ src/logics.ts
@@ -1,3 +1,9 @@
-function isRealServer(){
+
+/*
+@author : Lutica CANARD
+@description : Checking connect is from real server.
+*/
+export default function isRealServer():boolean 
+{
     return window.location.hostname === "localhost"?false:true;
 }
--- src/login.tsx
+++ src/login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import './logics'
+import isRealServer from './logics'
 
 const port = 4300;
 
