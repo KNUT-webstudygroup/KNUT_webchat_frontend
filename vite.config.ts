89588764f4706d68cb4fc64c665eb4b34fef3808
LuticaCANARD <presan100@gmail.com>
2023-05-03 15:06:46 +0900

    emergency fix : except SWC before fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+//import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  //plugins: [react()],
   build:{
     outDir:'docs'
   },
